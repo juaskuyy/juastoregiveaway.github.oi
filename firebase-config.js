f161d8e360a1f80677b99d7ef8c2fffee72dcb9b
juaskuyy <jhonyoga01@gmail.com>
2026-07-24 00:06:55 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Salin konfigurasi Firebase milikmu ke bagian ini.
-// Ambil dari Firebase Console → Project settings → Your apps → Web app.
 window.firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY",
+  apiKey: "AIzaSyCt8gPAPto9GX3cu-Btn9hbHoD8T28y7wU",
   authDomain: "juastore-25a81.firebaseapp.com",
   projectId: "juastore-25a81",
   storageBucket: "juastore-25a81.firebasestorage.app",
-  messagingSenderId: "GANTI_DENGAN_MESSAGING_SENDER_ID",
-  appId: "GANTI_DENGAN_APP_ID"
+  messagingSenderId: "874449568775",
+  appId: "1:874449568775:web:d3c2c57dd5c833eb30d6a2",
+  measurementId: "G-EE5JS6NCFC"
 };
